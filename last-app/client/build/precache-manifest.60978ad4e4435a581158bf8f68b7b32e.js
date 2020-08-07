2c196c518bdebe1263377a4ded092b132a1822af
Awselali77 <awselali77@gmail.com>
2020-08-07 15:38:10 +0300

    restructure for deployment

---
+++ last-app/client/build/precache-manifest.60978ad4e4435a581158bf8f68b7b32e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30fbd6b29d60927a698b9e2c625518e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2c80bfba328646ba5c0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aaeb73a22ce6e8fe8e42",
+    "url": "/static/css/main.5975f586.chunk.css"
+  },
+  {
+    "revision": "b2c80bfba328646ba5c0",
+    "url": "/static/js/2.fed11470.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fed11470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaeb73a22ce6e8fe8e42",
+    "url": "/static/js/main.aeb33dd3.chunk.js"
+  },
+  {
+    "revision": "51cdcb231ede1ffd47e6",
+    "url": "/static/js/runtime-main.6d92cdef.js"
+  },
+  {
+    "revision": "373c82a627bea5324f3406ed83cb5e32",
+    "url": "/static/media/logo.373c82a6.jpg"
+  }
+]);
