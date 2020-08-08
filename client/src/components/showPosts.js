0dd3eb8f2b562ce942127319dcf257f128b22b98
Awselali77 <awselali77@gmail.com>
2020-08-08 09:36:46 +0300

    card added

--- client/src/components/showPosts.js
+++ client/src/components/showPosts.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 import {Form} from './AuthForm';
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import ListItem from "@material-ui/core/ListItem"
 
 export default class ShowPosts extends Component {
   constructor(props) {
@@ -55,23 +58,37 @@ export default class ShowPosts extends Component {
 
           <br />
         </div>
-
+        <Grid>
 {
   
   this.state.data.map((item,index) => (
-      <Form key = {index+1}>
+    <Card className={{root: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: "inline-block",
+      margin: "0 2px",
+      transform: "scale(0.8)",
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    }}} variant="outlined">
     <ul key={index}>
-  <label key ={item.details}>{"details : " + item.details}</label><br/>
-  <label key ={item.phone} >{"phone number : " + item.phone  }</label><br/>
-  <label key ={item.place}>{"city : " + item.place}</label> <br/>
-  <label style={{ color: 'green' }} key ={item.price}>{"Price : " + Price(item.price)}</label><br/>
-  <label key ={item.createdAt}>{"puplished at : " + takeDate(item.createdAt) + " At " + takeTime(item.createdAt)}</label>
+  <ListItem key ={item.details}>{"details : " + item.details}</ListItem>
+  <ListItem key ={item.phone} >{"phone number : " + item.phone  }</ListItem>
+  <ListItem key ={item.place}>{"city : " + item.place}</ListItem> 
+  <ListItem style={{ color: 'green' }} key ={item.price}>{"Price : " + Price(item.price)}</ListItem>
+  <ListItem key ={item.createdAt}>{"puplished at : " + takeDate(item.createdAt) + " At " + takeTime(item.createdAt)}</ListItem>
 
- <br/><br/><br/>
+ 
  </ul> 
- </Form>
+ </Card>
  ))
 }
+</Grid>
       </form>
     );
   }
